46cf9ae102986734bdf6989273f3799f55636fe2
Jimmy Hwang <jimmy.hwang@hpe.com>
2019-06-24 16:55:40 +0800

    Add ListHDD()

--- Form1.cs
+++ Form1.cs
@@ -711,5 +711,37 @@ namespace NppLauncher {
         ConfigData = form.ConfigData;
       }
     }  
+
+    List<string> GetAllHDD() {
+      DriveInfo[] allDrives = DriveInfo.GetDrives();
+      List<string> HDDs = new List<string>();
+
+      foreach (DriveInfo d in allDrives) {
+        /*
+        Console.WriteLine("Drive {0}", d.Name);
+        Console.WriteLine("  Drive type: {0}", d.DriveType);
+        if (d.IsReady == true) {
+          Console.WriteLine("  Volume label: {0}", d.VolumeLabel);
+          Console.WriteLine("  File system: {0}", d.DriveFormat);
+          Console.WriteLine(
+              "  Available space to current user:{0, 15} bytes",
+              d.AvailableFreeSpace);
+
+          Console.WriteLine(
+              "  Total available space:          {0, 15} bytes",
+              d.TotalFreeSpace);
+
+          Console.WriteLine(
+              "  Total size of drive:            {0, 15} bytes ",
+              d.TotalSize);
+        }
+        */
+        if (d.DriveType.ToString() == "Fixed") {
+          HDDs.Add(d.Name);
+        }
+      }
+
+      return HDDs;
+    }
   } // Form1
 }
